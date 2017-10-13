e346e94b0859be761dffd704aa0940703a535736
nguyendh2 <ducnguyen1234567890@yahoo.com>
2017-10-13 16:50:53 -0500

    chinggone
    taking too long

---
+++ InfiniteCell/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfiniteCell.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
