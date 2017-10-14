43e798710900fdcfc852b445eec49a04c4eb9f35
nguyendh2 <ducnguyen1234567890@yahoo.com>
2017-10-14 16:16:08 -0500

    authenticated can see game

--- PseudoCell/Controllers/GameController.cs
+++ PseudoCell/Controllers/GameController.cs
@@ -1,20 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
-using PseudoCell.Models;
 using PseudoCell.DataAccess;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
 
 namespace PseudoCell.Controllers
 {
     public class GameController:Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public GameController()
+        {
+        }
+
+        public GameController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
         public ActionResult Index()
         {
             var context = new MyDataContext();
-            
-            return View(context.GameModels);
-            
+            if (User.Identity.IsAuthenticated)
+            {
+                //var user = UserManager.FindById(User.Identity.GetUserId());
+                //using (var userContext = new MyDataContext())
+                //{
+                //    var result = userContext.Users.FirstOrDefault(x => x.AspNetUserId.Equals(user.Id, StringComparison.OrdinalIgnoreCase));
+                //    if(result.IsManager || result.IsAdmin)
+                        return View(context.GameModels);
+                //}
+
+            }
+            return RedirectToAction("Index","Home");
         }
     }
 }
